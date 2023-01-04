6ab35380019a85c2b187dff4c6776acc16090672
aagii <altangerelmaakhuu60@gmail.com>
2023-01-04 16:49:23 +1100

    Delgetsees ugugdul avah code-uug bichih

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 // дэлгэцтэй ажиллах контроллер
 var uiController = (function() {
+    // Олон классуудыг Index.html-ээс сонгон авахад сүүлдээ алдаа гарвал асуудал үсэх тул тусад нь авах
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDesc: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    };
+    return {
+        getInput: function() {
+            return {
+                //Орлого зарлага алийг сонгосныг авах
+                type: document.querySelector(DOMstrings.inputType).value,
+                //Орлого зарлагын нэрийг авах
+                description: document.querySelector(DOMstrings.inputDesc).value,
+                //Үнийн дүнг авах
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+            
+        },
 
+        // addBtn өөр контроллер дотор тул уншиж чадахгүй тул шинэ БУЦААЛТ үүсгэж уншина
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 })();
 
 // Санххүтэй ажиллах контроллер
@@ -11,19 +35,24 @@ var financeController = (function() {
 // Програмын холбогч контролллер
 var appController = (function(uiController, financeController) {
 
+    //Шинэ БУЦААЛТАА уншихын тулд хувьсагчид өгөдлийг түүгээрээ дамжуулж өгөгдлөө уншиж авна.
+    var DOM = uiController.getDOMstrings();
+
     var ctrlAddItem = function() {
-        console.log('Дэлгэцээс өгөдлөө авах хэсэг.');
+        
+        
         // 1. Оруулах өгөдлийг дэлгэцээс олж авна
+        console.log(uiController.getInput());
         // 2. Олж авсан өгөгдлүүдээ Санхүүгийн контроллерт дамжуулж тэнд хадгална.
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     }
-
-    document.querySelector(".add__btn").addEventListener('click', function() {
+    //Товч дарахад хийх үйлдэл
+    document.querySelector(DOM.addBtn).addEventListener('click', function() {
         ctrlAddItem();
     });
-
+    // Enter дарахад хийх үйлдэл
     document.addEventListener('keypress', function(event) {
         //console.log(event);
         if(event.code === 13 || event.which === 13) {
